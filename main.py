5e42ebeb7be7fee347389617392bb0e3e1a68691
Tosh Camille <tochecamille@gmail.com>
2016-12-28 18:36:14 +0100

    support Exception

--- main.py
+++ main.py
@@ -14,24 +14,26 @@ if __name__ == '__main__':
 		t = TwitterApi(i)
 		dUser = {}
 		friends = []
-
+		try:
 		# je parcour les amis
-		for f in t.findFriends():
-			dUser['name'] = f['screen_name']
-			dUser['id'] = f['id']
+			for f in t.findFriends():
+				dUser['name'] = f['screen_name']
+				dUser['id'] = f['id']
 
-			# je parcour les amis des amis
-			try:
-				friendsOfFriends = []
+				# je parcour les amis des amis
+				try:
+					friendsOfFriends = []
 
-				for fof in t.findFriendsWithId(f['id']):
-					friendsOfFriends.append(fof['screen_name'])
-			
-				dUser['friends'] = friendsOfFriends
-			except Exception as e:
-				print(e)
+					for fof in t.findFriendsWithId(f['id']):
+						friendsOfFriends.append(fof['screen_name'])
+				
+					dUser['friends'] = friendsOfFriends
+				except Exception as e:
+					print(e)
 
-			friends.append(dUser)
-		d[t.check()['id']] = friends
+				friends.append(dUser)
+			d[t.check()['id']] = friends
+		except Exception as e:
+					print(e)
 
 	print(json.dumps(d))
